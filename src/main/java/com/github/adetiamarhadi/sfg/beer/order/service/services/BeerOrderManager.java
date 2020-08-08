1f978e17f337726f8186f61cd67988e67d2c834f
adetiamarhadi <adetia.marhadi@gmail.com>
2020-08-09 05:40:40 +0700

    Chapter 226 : Beer Order Manager Service

---
+++ src/main/java/com/github/adetiamarhadi/sfg/beer/order/service/services/BeerOrderManager.java
@@ -0,0 +1,7 @@
+package com.github.adetiamarhadi.sfg.beer.order.service.services;
+
+import com.github.adetiamarhadi.sfg.beer.order.service.domain.BeerOrder;
+
+public interface BeerOrderManager {
+    BeerOrder newBeerOrder(BeerOrder beerOrder);
+}
