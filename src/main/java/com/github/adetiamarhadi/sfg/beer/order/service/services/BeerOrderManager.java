2959ab208bdaf49c7111158808ba06a21d3d1e56
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2020-08-12 11:20:07 +0700

    Assignment - Complete Allocation Message Flow

--- src/main/java/com/github/adetiamarhadi/sfg/beer/order/service/services/BeerOrderManager.java
+++ src/main/java/com/github/adetiamarhadi/sfg/beer/order/service/services/BeerOrderManager.java
@@ -1,6 +1,7 @@
 package com.github.adetiamarhadi.sfg.beer.order.service.services;
 
 import com.github.adetiamarhadi.sfg.beer.order.service.domain.BeerOrder;
+import com.github.adetiamarhadi.sfg.brewery.model.BeerOrderDto;
 
 import java.util.UUID;
 
@@ -8,4 +9,10 @@ public interface BeerOrderManager {
     BeerOrder newBeerOrder(BeerOrder beerOrder);
 
     void processValidationResult(UUID beerOrderId, Boolean isValid);
+
+    void beerOrderAllocationPassed(BeerOrderDto beerOrderDto);
+
+    void beerOrderAllocationPendingInventory(BeerOrderDto beerOrderDto);
+
+    void beerOrderAllocationFailed(BeerOrderDto beerOrderDto);
 }
--- src/main/java/com/github/adetiamarhadi/sfg/beer/order/service/services/BeerOrderManagerImpl.java
+++ src/main/java/com/github/adetiamarhadi/sfg/beer/order/service/services/BeerOrderManagerImpl.java
@@ -5,6 +5,7 @@ import com.github.adetiamarhadi.sfg.beer.order.service.domain.BeerOrderEventEnum
 import com.github.adetiamarhadi.sfg.beer.order.service.domain.BeerOrderStatusEnum;
 import com.github.adetiamarhadi.sfg.beer.order.service.repositories.BeerOrderRepository;
 import com.github.adetiamarhadi.sfg.beer.order.service.services.sm.BeerOrderStateChangeInterceptor;
+import com.github.adetiamarhadi.sfg.brewery.model.BeerOrderDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
@@ -52,6 +53,44 @@ public class BeerOrderManagerImpl implements BeerOrderManager {
         } else {
             sendBeerOrderEvent(beerOrder, BeerOrderEventEnum.VALIDATION_FAILED);
         }
+    }
+
+    @Override
+    public void beerOrderAllocationPassed(BeerOrderDto beerOrderDto) {
+        BeerOrder beerOrder = this.beerOrderRepository.getOne(beerOrderDto.getId());
+        sendBeerOrderEvent(beerOrder, BeerOrderEventEnum.ALLOCATION_SUCCESS);
+        updateAllocateQty(beerOrderDto, beerOrder);
+    }
+
+    private void updateAllocateQty(BeerOrderDto beerOrderDto, BeerOrder beerOrder) {
+
+        BeerOrder allocatedOrder = this.beerOrderRepository.getOne(beerOrderDto.getId());
+
+        allocatedOrder.getBeerOrderLines().forEach(beerOrderLine -> {
+            beerOrderDto.getBeerOrderLines().forEach(beerOrderLineDto -> {
+                if (beerOrderLine.getId().equals(beerOrderLineDto.getId())) {
+                    beerOrderLine.setQuantityAllocated(beerOrderLineDto.getQuantityAllocated());
+                }
+            });
+        });
+
+        this.beerOrderRepository.saveAndFlush(beerOrder);
+    }
+
+    @Override
+    public void beerOrderAllocationPendingInventory(BeerOrderDto beerOrderDto) {
+        BeerOrder beerOrder = this.beerOrderRepository.getOne(beerOrderDto.getId());
+
+        sendBeerOrderEvent(beerOrder, BeerOrderEventEnum.ALLOCATION_NO_INVENTORY);
+
+        updateAllocateQty(beerOrderDto, beerOrder);
+    }
+
+    @Override
+    public void beerOrderAllocationFailed(BeerOrderDto beerOrderDto) {
+        BeerOrder beerOrder = this.beerOrderRepository.getOne(beerOrderDto.getId());
+
+        sendBeerOrderEvent(beerOrder, BeerOrderEventEnum.ALLOCATION_FAILED);
     }
 
     private void sendBeerOrderEvent(BeerOrder beerOrder, BeerOrderEventEnum eventEnum) {
