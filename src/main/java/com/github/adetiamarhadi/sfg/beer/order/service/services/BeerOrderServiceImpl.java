44061921b6edfcf2443364e2a2a6f1023b0e1a2d
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2020-08-14 10:38:17 +0700

    Chapter 240 : Refactor Beer Order Service to Use Saga

--- src/main/java/com/github/adetiamarhadi/sfg/beer/order/service/services/BeerOrderServiceImpl.java
+++ src/main/java/com/github/adetiamarhadi/sfg/beer/order/service/services/BeerOrderServiceImpl.java
@@ -17,16 +17,16 @@
 
 package com.github.adetiamarhadi.sfg.beer.order.service.services;
 
-import com.github.adetiamarhadi.sfg.beer.order.service.domain.BeerOrderStatusEnum;
-import com.github.adetiamarhadi.sfg.beer.order.service.web.mappers.BeerOrderMapper;
-import com.github.adetiamarhadi.sfg.brewery.model.BeerOrderDto;
-import com.github.adetiamarhadi.sfg.brewery.model.BeerOrderPagedList;
 import com.github.adetiamarhadi.sfg.beer.order.service.domain.BeerOrder;
+import com.github.adetiamarhadi.sfg.beer.order.service.domain.BeerOrderStatusEnum;
 import com.github.adetiamarhadi.sfg.beer.order.service.domain.Customer;
 import com.github.adetiamarhadi.sfg.beer.order.service.repositories.BeerOrderRepository;
 import com.github.adetiamarhadi.sfg.beer.order.service.repositories.CustomerRepository;
+import com.github.adetiamarhadi.sfg.beer.order.service.web.mappers.BeerOrderMapper;
+import com.github.adetiamarhadi.sfg.brewery.model.BeerOrderDto;
+import com.github.adetiamarhadi.sfg.brewery.model.BeerOrderPagedList;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -38,22 +38,14 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class BeerOrderServiceImpl implements BeerOrderService {
 
     private final BeerOrderRepository beerOrderRepository;
     private final CustomerRepository customerRepository;
     private final BeerOrderMapper beerOrderMapper;
-    private final ApplicationEventPublisher publisher;
-
-    public BeerOrderServiceImpl(BeerOrderRepository beerOrderRepository,
-                                CustomerRepository customerRepository,
-                                BeerOrderMapper beerOrderMapper, ApplicationEventPublisher publisher) {
-        this.beerOrderRepository = beerOrderRepository;
-        this.customerRepository = customerRepository;
-        this.beerOrderMapper = beerOrderMapper;
-        this.publisher = publisher;
-    }
+    private final BeerOrderManager beerOrderManager;
 
     @Override
     public BeerOrderPagedList listOrders(UUID customerId, Pageable pageable) {
@@ -88,12 +80,9 @@ public class BeerOrderServiceImpl implements BeerOrderService {
 
             beerOrder.getBeerOrderLines().forEach(line -> line.setBeerOrder(beerOrder));
 
-            BeerOrder savedBeerOrder = beerOrderRepository.saveAndFlush(beerOrder);
+            BeerOrder savedBeerOrder = this.beerOrderManager.newBeerOrder(beerOrder);
 
             log.debug("Saved Beer Order: " + beerOrder.getId());
-
-            //todo impl
-          //  publisher.publishEvent(new NewBeerOrderEvent(savedBeerOrder));
 
             return beerOrderMapper.beerOrderToDto(savedBeerOrder);
         }
@@ -108,10 +97,7 @@ public class BeerOrderServiceImpl implements BeerOrderService {
 
     @Override
     public void pickupOrder(UUID customerId, UUID orderId) {
-        BeerOrder beerOrder = getOrder(customerId, orderId);
-        beerOrder.setOrderStatus(BeerOrderStatusEnum.PICKED_UP);
-
-        beerOrderRepository.save(beerOrder);
+        this.beerOrderManager.beerOrderPickedUp(orderId);
     }
 
     private BeerOrder getOrder(UUID customerId, UUID orderId){
